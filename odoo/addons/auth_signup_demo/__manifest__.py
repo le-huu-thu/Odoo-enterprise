54198da5ff53472b229d013042eb412297c4a56e
le-huu-thu <lehuuthu06062000@gmail.com>
2024-11-27 18:34:54 +0700

    Initial commit

---
+++ odoo/addons/auth_signup_demo/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': "Signup Demo",
+    'version': "18.0",
+    'summary': "Odoo Demo",
+    'description': "Odoo Demo",
+    'author': "Adil Akbar",
+    'category': "Custom",
+    'depends': ['auth_signup'],
+    'data': [
+        'auth_signup_template.xml'
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'application': True,
+}
